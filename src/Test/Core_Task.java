bc1c1557e429e8e8325865c55a7f1a29eb797c81
nykolyshyn danylo <danylo.nykolyshyn@gmail.com>
2022-04-11 19:39:31 +0300

    start test

--- src/Test/Core_Task.java
+++ src/Test/Core_Task.java
@@ -4,18 +4,37 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Core_Task {
     public static final String path = "/Users/khrystyna_nykolyshyn/Documents/TestingLogos/";
     public static final File file = new File("1.txt");
     public static void main(String[] args) {
         System.out.println("hello igor )");
-
+        InputOutput inputOutput = new InputOutput();
+        List<Device> list = new ArrayList<>();
+        list.add(new Device(1213L,"lg","info",21321));
+        try {
+            inputOutput.serialize(list);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            inputOutput.deserialize(list);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
+
 @Data
 @AllArgsConstructor
-class Device{
+class Device implements Serializable {
     Long seriaNumber;
     String brend;
     String info;
--- src/Test/InputOutput.java
+++ src/Test/InputOutput.java
@@ -1,5 +1,26 @@
 package Test;
 
+import java.io.*;
+import java.util.List;
+
 public class InputOutput {
+    public static final String path = "/Users/khrystyna_nykolyshyn/Documents/TestingLogos/";
+    public static final File file = new File("1.txt");
+    public List<Device> deserialize(List<Device> deviceList) throws IOException, ClassNotFoundException {
+        FileInputStream inputStream = new FileInputStream(path + File.separator + file);
+        ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+        List<Device> list = (List<Device>) objectInputStream.readObject();
+        System.out.println(list.toString());
+        System.out.println(deviceList);
+        inputStream.close();
+        return  list;
+    }
+    public void serialize(List<Device> list) throws IOException {
+        FileOutputStream outputStream = new FileOutputStream(path + File.separator + file);
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+        objectOutputStream.writeObject(list);
+        objectOutputStream.flush();
+        objectOutputStream.close();
+    }
 
 }

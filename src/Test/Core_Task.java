5f88d43b54b245c8670a5de2289530a7933df315
nykolyshyn danylo <danylo.nykolyshyn@gmail.com>
2022-04-11 19:16:03 +0300

    start test

--- src/Test/Core_Task.java
+++ src/Test/Core_Task.java
@@ -1,8 +1,23 @@
 package Test;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.File;
+
 public class Core_Task {
+    public static final String path = "/Users/khrystyna_nykolyshyn/Documents/TestingLogos/";
+    public static final File file = new File("1.txt");
     public static void main(String[] args) {
         System.out.println("hello igor )");
 
     }
+}
+@Data
+@AllArgsConstructor
+class Device{
+    Long seriaNumber;
+    String brend;
+    String info;
+    int price;
 }

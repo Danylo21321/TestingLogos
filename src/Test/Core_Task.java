0b360d3a6ea11c456ed884d917a1a92a294f37ac
nykolyshyn danylo <danylo.nykolyshyn@gmail.com>
2022-04-11 20:59:03 +0300

    start test

--- src/Test/Core_Task.java
+++ src/Test/Core_Task.java
@@ -4,23 +4,75 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.io.File;
-import java.io.IOException;
 import java.io.Serializable;
-import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Scanner;
 
 public class Core_Task {
     public static final String path = "/Users/khrystyna_nykolyshyn/Documents/TestingLogos/";
     public static final File file = new File("1.txt");
+    public static List<Device> list = List.of(new Device(2L,"lg","info",401)
+    , new Device(3L,"samsung","info",200));
+    public static  Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        List<Device> list = new ArrayList<>();
-        list.add(new Device(1213L,"lg","info",21321));
         Builder builder = new RealizeIo();
         builder.serializeAndDeSerialize(list).writer(list);
-        InputOutput inputOutput1 = new InputOutput();
-
+        AllOperationWithDevice();
     }
-}
+    public static void showAllDevice(){
+        list.stream().forEach(System.out::println);
+    }
+    public static void sortDeviceWithBrend(){
+        list.stream().sorted(Comparator.comparing(Device::getBrend)).forEach(System.out::println);
+    }
+    public static void sechDevicrWithNumber(){
+        long serialNumberDevice = scanner.nextLong();
+        list.stream().filter(device -> device.getSeriaNumber() == serialNumberDevice).forEach(System.out::println);
+    }
+    public static void filterDeviceWithSum(){
+        list.stream().filter(device -> device.getPrice() > 400).forEach(System.out::println);
+    }
+    public static void sechMaxDeviceForPrice(){
+        Device max = list.stream().max(Comparator.comparingDouble(Device::getPrice)).get();
+        System.out.println(max);
+    }
+    public static void showHighPriceDevice(){
+        list.stream().mapToInt(Device::getPrice).max().ifPresent(System.out::println);
+    }
+    public static void AllOperationWithDevice(){
+        while (true){
+            String forSwitch = scanner.next();
+            switch (forSwitch){
+                case "1":
+                    showAllDevice();
+                    break;
+                case "2":
+                    sortDeviceWithBrend();
+                    break;
+                case "3":
+                    sechDevicrWithNumber();
+                    break;
+                case "4":
+                    filterDeviceWithSum();
+                case "5":
+                    sechMaxDeviceForPrice();
+                    break;
+                case "6":
+                    sechMaxDeviceForPrice();
+                    break;
+                case"7":
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("incorrect answer");
+
+            }
+        }
+    }
+    }
+
 
 @Data
 @AllArgsConstructor
